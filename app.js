0edb4d4f8cd52e9d6480f0a69cef07d08cdf2fbf
ismaila sylla <syldox@gmail.com>
2017-02-18 21:29:13 +0530

    updates

---
+++ app.js
@@ -0,0 +1,16 @@
+(function(){
+
+    'use strict';
+
+    angular.module('myApp', [])
+    .controller('myFirstController', function($scope){
+
+        $scope.name = "Ismaila";
+
+
+
+    });
+
+
+
+})();
